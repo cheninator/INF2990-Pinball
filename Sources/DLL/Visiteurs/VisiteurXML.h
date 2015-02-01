c909b249eca0485bb6899dd8222ec866b807c2df
cheninator <yonni.chen-kuang-piao@polymtl.ca>
2015-02-01 18:09:43 -0400

    VisiteurXML flawless
    La classe fonctionne parfaitement. L'écriture en fichier XML a été testé
    aussi.

--- Sources/DLL/Visiteurs/VisiteurXML.h
+++ Sources/DLL/Visiteurs/VisiteurXML.h
@@ -1,15 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file VisiteurXML.h
+/// @author The Ballers
+/// @date 2015-02-01
+/// @version 1.0
+///
+/// @addtogroup inf2990 INF2990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
 #ifndef __VISITEURS_VISITEURXML_H__
 #define __VISITEURS_VISITEURXML_H__
 
 #include "VisiteurAbstrait.h"
 #include "tinyxml2.h"
-#include <iostream>
+#include <string>
 
+///////////////////////////////////////////////////////////////////////////
+/// @class VisiteurXML
+/// @brief Classe pour enregistrer l'information de l'arbre de rendu
+///		¸  dans un fichier XML.
+///
+///        Instancier cette classe avec en paramètre le nom du fichier avec
+///		   l'extension .xml . Ensuite, appelez la méthode accepterVisiteur()
+///		   de l'arbre de rendu et lui fournir CE visiteur en paramètre
+///
+/// @author Yonni Chen
+/// @date 2015-02-02
+///////////////////////////////////////////////////////////////////////////
 class VisiteurXML : public VisiteurAbstrait
 {
 public:
 
-	// Constructeur par défaut : aucun déplacement
+	// Constructeur par paramètre : fournir le nom du fichier de sauvegarde
 	VisiteurXML(std::string nomFichier);
 
 	// Destructeur
@@ -21,10 +42,13 @@ public:
 
 private:
 
+	// Ne touchez pas au constructeur par défaut
+	VisiteurXML();
+
 	tinyxml2::XMLDocument document;
 	std::string nomFichier;
 	
 };
 
 
-#endif //__VISITEURS_VISITEURSELECTION_H__
+#endif //__VISITEURS_VISITEURXML_H__
