86b3906ba0539290c47c1fb325e1234c0f3dcf8f
Pitcher666 <kimpich@gmail.com>
2015-02-06 14:46:23 -0500

    ajout visiteur selection multiple

---
+++ Sources/DLL/VisiteurSelectionMultiple.cpp
@@ -0,0 +1,132 @@
+////////////////////////////////////////////////
+/// @file   VisiteurSelection.cpp
+/// @author The Ballers
+/// @date   2015-02-01
+///
+/// @addtogroup inf2990 INF2990
+/// @{
+////////////////////////////////////////////////
+#include "VisiteurSelectionMultiple.h"
+#include "../Arbre/ArbreRenduINF2990.h"
+#include "../Arbre/Noeuds/NoeudTable.h"
+#include <iostream>
+#include "Utilitaire.h"
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn VisiteurSelection::VisiteurSelection()
+///
+/// VIDE
+///
+/// @return Aucune (constructeur).
+///
+////////////////////////////////////////////////////////////////////////
+VisiteurSelectionMultiple::VisiteurSelectionMultiple()
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn VisiteurDeplacement::VisiteurSelection(glm::dvec3 pointDansLeMonde)
+///
+/// Ne fait qu'initialiser les variables membres de la classe.
+///
+/// @param[in] dev : Le vecteur de deplacement
+///
+/// @return Aucune (constructeur).
+///
+////////////////////////////////////////////////////////////////////////
+VisiteurSelectionMultiple::VisiteurSelectionMultiple(glm::dvec3 selectionBasGauche, glm::dvec3 selectionHautDroit)
+:nbObjetsSelectionne_{ 0 }
+{
+	selectionBasGauche_ = selectionBasGauche;
+	selectionHautDroit_ = selectionHautDroit;
+}
+
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn VisiteurSelection::~VisiteurSelection()
+///
+/// Destructeur vide 
+///
+/// @return Aucune (destructeur).
+///
+////////////////////////////////////////////////////////////////////////
+VisiteurSelectionMultiple::~VisiteurSelectionMultiple()
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn bool VisiteurSelection::traiter(ArbreRenduINF2990* noeud)
+///
+/// Cette fonction traite l'arbre de rendu pour selectionner ses enfants
+///
+/// Cette fonction retourne true pour dire que l'opération s'est
+/// fait correctement
+///
+/// @return Retourne toujours true
+///
+////////////////////////////////////////////////////////////////////////
+bool VisiteurSelectionMultiple::traiter(ArbreRenduINF2990* arbre)
+{
+	// Visiter les enfants de l'arbre
+	for (unsigned int i = 0; i < arbre->obtenirNombreEnfants(); i++)
+	{
+		// Traiter les enfants de l'arbre de rendu
+		arbre->getEnfant(i)->accepterVisiteur(this);
+	}
+
+	return true;
+}
+
+bool VisiteurSelectionMultiple::traiter(NoeudTable* table)
+{;
+	// Traiter les enfants selectionnés de la table
+	for (unsigned int i = 0; i < table->obtenirNombreEnfants(); i++)
+	{
+		table->getEnfant(i)->accepterVisiteur(this);
+	}
+
+	return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn bool VisiteurSelection::traiter(NoeudAbstrait* noeud)
+///
+/// Cette fonction traite les enfants de l'arbre de rendu. Si ses enfants ont des enfants
+/// ils seront aussi traités.
+///
+/// Cette fonction retourne true pour dire que l'opération s'est
+/// fait correctement
+///
+/// @return Retourne toujours true
+///
+////////////////////////////////////////////////////////////////////////
+bool VisiteurSelectionMultiple::traiter(NoeudAbstrait* noeud)
+{
+	std::cout << "visite d'un noeudAbstrait avec identifiant " << noeud->getNumero() << std::endl;
+
+	glm::dvec3 origineNoeud = noeud->obtenirPositionRelative();
+
+	if (utilitaire::DANS_LIMITESXY(origineNoeud.x, selectionBasGauche_.x, selectionBasGauche_.x,
+		                           origineNoeud.y, selectionBasGauche_.y, selectionBasGauche_.y))
+	{
+		std::cout << "Noeud de type " << noeud->getType() << " selectionne " << std::endl;
+		noeud->assignerSelection(true);
+		nbObjetsSelectionne_++;
+	}
+	else
+	{
+		noeud->assignerSelection(false);
+	}
+
+	return true;
+}
---
+++ Sources/DLL/VisiteurSelectionMultiple.h
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file VisiteurSelection.h
+/// @author The Ballers
+/// @date 2015-02-01
+/// @version 1.0
+///
+/// @addtogroup inf2990 INF2990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
+#ifndef __VISITEURS_VISITEURSELECTIONMULTIPLE_H__
+#define __VISITEURS_VISITEURSELECTIONMULTIPLE_H__
+
+#include "NoeudAbstrait.h"
+#include "NoeudComposite.h"
+#include "Visiteurs/VisiteurAbstrait.h"
+#include "glm\glm.hpp"
+
+///////////////////////////////////////////////////////////////////////////
+/// @class VisiteurSelection
+/// @brief Classe pour selectionner les objets de l'arbre de rendu
+///
+///        Instancier cette classe avec en paramètre le point cliqué
+///		   Ensuite, appelez la méthode accepterVisiteur() de l'arbre de 
+///		   rendu et lui fournir CE visiteur en paramètre
+///
+/// @author Yonni Chen
+/// @date 2015-02-02
+///////////////////////////////////////////////////////////////////////////
+class VisiteurSelectionMultiple : public VisiteurAbstrait
+{
+public:
+
+	// Constructeur par défaut : aucun déplacement
+	VisiteurSelectionMultiple(glm::dvec3 selectionBasGauche, glm::dvec3 selectionHautDroit);
+
+	// Destructeur
+	~VisiteurSelectionMultiple();
+
+	// Traiter une opération sur l'arbre de rendu et ses enfants
+	virtual bool traiter(ArbreRenduINF2990* arbre);
+	virtual bool traiter(NoeudAbstrait* noeud);
+	virtual bool traiter(NoeudTable* table);
+
+	int obtenirNbObjetsSelectionne(){ return nbObjetsSelectionne_; }
+
+private:
+
+	// Ne touchez pas au constructeur par défaut
+	VisiteurSelectionMultiple();
+
+	// Point cliqué
+	glm::dvec3 selectionBasGauche_, selectionHautDroit_;
+
+	int nbObjetsSelectionne_;
+};
+
+
+#endif //__VISITEURS_VISITEURSELECTIONMULTIPLE_H__
