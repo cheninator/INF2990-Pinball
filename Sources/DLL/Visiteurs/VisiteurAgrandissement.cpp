2275f7874a0de65b4bc63322c37861d2fb615e33
PewMcDaddy <phil103@hotmail.com>
2015-02-15 19:44:47 -0500

    Changé la façon de faire un scale.
    Le visiteur scale s'occuppe que le top de l'objet reste à la même hauteur qu'au départ.
    
    Pour le squelette, vu que son nez était à une hauteur fixe, le grossir le faisait rentrer dans la table.  Maintenant, le visiteur fait un traitement spécial sur celui-ci: si on est dans les scale plus petit que 1, on fait le truc ou on garde son top à la même hauteur, et si on est dans les scale plus petit que 1, on ne fait pas l'affaire avec le top.

--- Sources/DLL/Visiteurs/VisiteurAgrandissement.cpp
+++ Sources/DLL/Visiteurs/VisiteurAgrandissement.cpp
@@ -10,6 +10,7 @@
 #include "../Arbre/ArbreRenduINF2990.h"
 #include "../Arbre/Noeuds/NoeudTable.h"
 #include "../Arbre/Noeuds/NoeudMur.h"
+#include "../Arbre/Noeuds/NoeudGenerateurBille.h"
 #include <iostream>
 
 
@@ -133,10 +134,27 @@ bool VisiteurAgrandissement::traiter(NoeudAbstrait* noeud)
 		// c'est pour ça qu'il y a une exponentielle dans la fonction de facadeModele qui calcule le scale à donner au visiteur.
 		glm::dvec3 scaleInit = noeud->obtenirAgrandissement();
 		glm::dvec3 scaleFinal = glm::dvec3{ scaleInit[0] * homothetie_[0], scaleInit[1] * homothetie_[1], scaleInit[2] * homothetie_[2] };
+	
+	
+
+		glm::dvec3 position = noeud->obtenirPositionRelative();
+		glm::dvec3 boite[4];
+		noeud->obtenirVecteursBoite(boite[0], boite[1], boite[2], boite[3]); // Je sais que c<est pas beau, le 3e, c'est le point le plus haut de la boite englobante
+		double zTop = boite[0].z;
+		for (int i = 1; i < 4; i++) // Trouver le point le plus haut de la boite englobante.  
+		{
+			if (zTop < boite[i].z)
+				zTop = boite[i].z;
+		}
+		double distancePositionTop = zTop;
+		double correctionZ = (1 - homothetie_[0]) * distancePositionTop; // Homothetie[0] est de l'ordre de 1.003, donc (1-homothetie[0]) est petie, donc la correction en z est petite.
+		position += glm::dvec3{ 0, 0, correctionZ };
+		// std::cout << "AVANT position.z : " << position.z << "   distancePositionTop : " << distancePositionTop << "    TOP : " << position.z + zTop << std::endl;
 		noeud->assignerEchelle(scaleFinal);
 		
+		noeud->assignerPositionRelative(position);
+		
 	}
-
 	return true;
 }
 
@@ -167,5 +185,38 @@ bool VisiteurAgrandissement::traiter(NoeudMur* noeud)
 		noeud->assignerEchelle(scaleFinal);
 	}
 
+	return true;
+}
+
+bool VisiteurAgrandissement::traiter(NoeudGenerateurBille* noeud)
+{
+
+	if (noeud->estSelectionne() && noeud->estAjustable() && noeud->estModifiable())
+	{
+		// LOGIQUE D'AGRANDISSEMENT
+		// REMARQUE: LE SCALE EST QUELQUE CHOSE DE MULTIPLICATIF
+		// et on utilise un déplacement pour la définir, qui est "additif". 
+		// Qu'est-ce qui transforme l'addition en multiplication? A^(x + y) = (A^x) * (A^y)
+		// c'est pour ça qu'il y a une exponentielle dans la fonction de facadeModele qui calcule le scale à donner au visiteur.
+		glm::dvec3 scaleInit = noeud->obtenirAgrandissement();
+		glm::dvec3 scaleFinal = glm::dvec3{ scaleInit[0] * homothetie_[0], scaleInit[1] * homothetie_[1], scaleInit[2] * homothetie_[2] };
+
+		glm::dvec3 position = noeud->obtenirPositionRelative();
+		glm::dvec3 boite[4];
+		noeud->obtenirVecteursBoite(boite[0], boite[1], boite[2], boite[3]); // Je sais que c<est pas beau, le 3e, c'est le point le plus haut de la boite englobante
+		double zTop = boite[0].z;
+		for (int i = 1; i < 4; i++) // Trouver le point le plus haut de la boite englobante.  
+		{
+			if (zTop < boite[i].z)
+				zTop = boite[i].z;
+		}
+		double distancePositionTop = zTop;
+		double correctionZ = (1 - homothetie_[0]) * distancePositionTop; // Homothetie[0] est de l'ordre de 1.003, donc (1-homothetie[0]) est petie, donc la correction en z est petite.
+		position += glm::dvec3{ 0, 0, correctionZ };
+		// std::cout << "AVANT position.z : " << position.z << "   distancePositionTop : " << distancePositionTop << "    TOP : " << position.z + zTop << std::endl;
+		noeud->assignerEchelle(scaleFinal);
+		if (scaleFinal.x * noeud->obtenirAgrandissement().x < 1)
+			noeud->assignerPositionRelative(position);
+	}
 	return true;
 }
