53c5aa84ea16c7a01d29a0cc8feb5f125df9b7f8
Aymen Djellal <aymendjellal@gmail.com>
2015-03-01 01:45:41 -0500

    Deplacement de code vers Program.cs
    Ménage du code
    (la fonction FullScreen et les FonctionsNatives sont maintenant dans
    Program.cs pour aleger le code)
    Virer plein de using useless, etc.

--- Sources/InterfaceGraphique/Campagne.cs
+++ Sources/InterfaceGraphique/Campagne.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
 
 
 namespace InterfaceGraphique
 {
-    
     public partial class Campagne : Form
     {
         string[] filePaths;
--- Sources/InterfaceGraphique/MenuPrincipal.cs
+++ Sources/InterfaceGraphique/MenuPrincipal.cs
@@ -8,18 +8,10 @@
 //////////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Media;
 using System.Windows.Forms;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 
 namespace InterfaceGraphique
 {
