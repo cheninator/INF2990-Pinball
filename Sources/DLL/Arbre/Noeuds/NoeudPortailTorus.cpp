b9ce001dcce8a169bb718e2e5d9b37f4299e5a25
PewMcDaddy <phil103@hotmail.com>
2015-03-07 10:27:04 -0500

    Remis la version finale de l'animation du tore autour du portail

--- Sources/DLL/Arbre/Noeuds/NoeudPortailTorus.cpp
+++ Sources/DLL/Arbre/Noeuds/NoeudPortailTorus.cpp
@@ -34,6 +34,9 @@
 NoeudPortailTorus::NoeudPortailTorus(const std::string& typeNoeud)
 	: NoeudComposite{ typeNoeud }
 {
+	scaleTorus_ = 1.0;
+	
+	direction_ = 0.995;
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -96,15 +99,12 @@ void NoeudPortailTorus::animer(float temps)
 {
 	NoeudComposite::animer(temps);
 
-	static double scaleTorus = 1.0;
-	static double direction = 0.995;
-
-	if (scaleTorus < 0.5)
-		direction = 1.007;
-	else if (scaleTorus > 1)
-		direction = 0.993;
-	scaleTorus = scaleTorus * direction;
-	assignerEchelle({ scaleTorus, scaleTorus, 1 });
+	if (scaleTorus_ < 0.5)
+		direction_ = 1.007;
+	else if (scaleTorus_ > 1)
+		direction_ = 0.993;
+	scaleTorus_ = scaleTorus_ * direction_;
+	assignerEchelle({ scaleTorus_, scaleTorus_, 1 });
 
 }
 
--- Sources/DLL/Arbre/Noeuds/NoeudPortailTorus.h
+++ Sources/DLL/Arbre/Noeuds/NoeudPortailTorus.h
@@ -38,6 +38,11 @@ public:
 
 	/// Accepte un visiteur.
 	virtual bool accepterVisiteur(VisiteurAbstrait* vis);
+
+private:
+	double scaleTorus_;
+	
+	double direction_;
 };
 
 #endif // __ARBRE_NOEUDS_NOEUDPORTAILTORUS_H__
