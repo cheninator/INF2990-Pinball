f460044f218e5246c3ec401cdb171fe328edf980
cheninator <yonni.chen-kuang-piao@polymtl.ca>
2015-03-08 20:41:47 -0300

    Ajout de commentaires et modification d'une méthode du singleton

--- Sources/DLL/Global/SingletonGlobal.cpp
+++ Sources/DLL/Global/SingletonGlobal.cpp
@@ -9,6 +9,17 @@
 #include "SingletonGlobal.h"
 
 
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn SingletonGlobal* SingletonGlobal::obtenirInstance()
+///
+/// Cette fonction retourne l'instance unique de la classe. Si l'instance
+/// n'existe pas, elle est créée. Ainsi, une seule instance sera créée.
+/// Cette fonction n'est pas "thread-safe".
+///
+/// @return L'instance unique de la classe.
+///
+////////////////////////////////////////////////////////////////////////
 SingletonGlobal* SingletonGlobal::obtenirInstance()
 {
 	if (singleton_ == nullptr)
@@ -17,7 +28,18 @@ SingletonGlobal* SingletonGlobal::obtenirInstance()
 	return singleton_;
 }
 
-SingletonGlobal::~SingletonGlobal()
+
+////////////////////////////////////////////////////////////////////////
+///
+/// @fn void SingletonGlobal::libererInstance()
+///
+/// Détruit l'instance unique de la classe.  Cette fonction n'est pas
+/// "thread-safe".
+///
+/// @return Aucune.
+///
+////////////////////////////////////////////////////////////////////////
+void SingletonGlobal::libererInstance()
 {
 	delete singleton_;
 	singleton_ = 0;
