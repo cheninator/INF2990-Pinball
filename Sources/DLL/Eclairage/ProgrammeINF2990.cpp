7f5950539388f6bb9694b106cdaa5e434979ed10
PewMcDaddy <phil103@hotmail.com>
2015-04-01 16:28:34 -0400

    Cleanup dans ProgrammeINF2990.cpp

--- Sources/DLL/Eclairage/ProgrammeINF2990.cpp
+++ Sources/DLL/Eclairage/ProgrammeINF2990.cpp
@@ -6,24 +6,25 @@ void ProgrammeINF2990::initialiser()
 	// Initialiser le nuanceur de sommets
 	std::cout << "============================  SOMMETS ====================== " << std::endl;
 	nuanceurSommets_ = new opengl::Nuanceur();
-	nuanceurSommets_->initialiser(opengl::Nuanceur::Type::NUANCEUR_VERTEX, "NuanceurSommetsINF2990.glsl");
+	nuanceurSommets_->initialiser(opengl::Nuanceur::Type::NUANCEUR_VERTEX, "Nuanceurs/NuanceurSommetsINF2990.glsl");
 	std::cout << nuanceurSommets_->serialiser();
 	// Initialiser le nuanceur de fragments
 	std::cout << "============================  FRAGMENTS ====================== " << std::endl;
 	nuanceurFragments_ = new opengl::Nuanceur();
-	nuanceurFragments_->initialiser(opengl::Nuanceur::Type::NUANCEUR_FRAGMENT, "");
+	nuanceurFragments_->initialiser(opengl::Nuanceur::Type::NUANCEUR_FRAGMENT, "Nuanceurs/NuanceurFragmentsINF2990.glsl");
 	std::cout << nuanceurFragments_->serialiser();
 
 	// Initialiser le programme
 	std::cout << "============================  PROGRAMME ====================== " << std::endl;
 	programme_ = new opengl::Programme;
+	programme_->initialiser();
 	programme_->attacherNuanceur(*nuanceurSommets_);
 	programme_->attacherNuanceur(*nuanceurFragments_);
 
 
 
 	std::cout << "in fct ProgrammeINF2990::initialiser() -- programme_->estValide() : " << programme_->estValide() << std::endl;
-	programme_->initialiser();
+	
 	std::cout << programme_->serialiser();
 	// Checker que tout est correct
 	if (0)
