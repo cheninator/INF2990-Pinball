bb9b66d4c7e41e1f3b85cbffa997c086e227f57f
PewMcDaddy <phil103@hotmail.com>
2015-04-01 16:30:16 -0400

    Cleanup dans ProgrammeINF2990.cpp, j'avais oublié de saver

--- Sources/DLL/Eclairage/ProgrammeINF2990.cpp
+++ Sources/DLL/Eclairage/ProgrammeINF2990.cpp
@@ -4,40 +4,36 @@
 void ProgrammeINF2990::initialiser()
 {
 	// Initialiser le nuanceur de sommets
-	std::cout << "============================  SOMMETS ====================== " << std::endl;
 	nuanceurSommets_ = new opengl::Nuanceur();
 	nuanceurSommets_->initialiser(opengl::Nuanceur::Type::NUANCEUR_VERTEX, "Nuanceurs/NuanceurSommetsINF2990.glsl");
-	std::cout << nuanceurSommets_->serialiser();
+	// std::cout << "============================  SOMMETS ====================== " << std::endl;
+	// std::cout << nuanceurSommets_->serialiser();
+	
 	// Initialiser le nuanceur de fragments
-	std::cout << "============================  FRAGMENTS ====================== " << std::endl;
 	nuanceurFragments_ = new opengl::Nuanceur();
 	nuanceurFragments_->initialiser(opengl::Nuanceur::Type::NUANCEUR_FRAGMENT, "Nuanceurs/NuanceurFragmentsINF2990.glsl");
-	std::cout << nuanceurFragments_->serialiser();
+	// std::cout << "============================  FRAGMENTS ====================== " << std::endl;
+	// std::cout << nuanceurFragments_->serialiser();
 
 	// Initialiser le programme
-	std::cout << "============================  PROGRAMME ====================== " << std::endl;
 	programme_ = new opengl::Programme;
 	programme_->initialiser();
 	programme_->attacherNuanceur(*nuanceurSommets_);
 	programme_->attacherNuanceur(*nuanceurFragments_);
-
-
-
-	std::cout << "in fct ProgrammeINF2990::initialiser() -- programme_->estValide() : " << programme_->estValide() << std::endl;
+	// std::cout << "============================  PROGRAMME ====================== " << std::endl;
+	//std::cout << programme_->serialiser();
 	
-	std::cout << programme_->serialiser();
 	// Checker que tout est correct
-	if (0)
+	if (programme_->estValide() && programme_->estInitialise())
 		EstInitialise_ = true;
-	std::cout << "============================ FIN INFO ====================== " << std::endl;
+	// std::cout << "============================ FIN INFO ====================== " << std::endl;
 
 }
 
 
 void ProgrammeINF2990::activer()
 {
-	// cout Phil
-	std::cout <<"Etat de validite de progNuanceur: "<< programme_->estValide() << std::endl;
+	// std::cout <<"Etat de validite de progNuanceur: "<< programme_->estValide() << std::endl;
 	opengl::Programme::Start(*programme_);
 }
 
