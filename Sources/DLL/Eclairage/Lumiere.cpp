87f4fc2a940bd163549b46344ddc2a3a98fc3ada
PewMcDaddy <phil103@hotmail.com>
2015-04-03 08:47:25 -0400

    Valeurs par defaut dans le constructeur de lumiere, le ControleurLumieres s'occuppe de configurer les lumieres.

--- Sources/DLL/Eclairage/Lumiere.cpp
+++ Sources/DLL/Eclairage/Lumiere.cpp
@@ -26,18 +26,18 @@ Lumiere::Lumiere(GLuint handle)
 	handle_ = handle;
 
 	// Couleurs qui font pas de sens mais sont utiles pour tester
-	ambient_ = glm::fvec4{ 1.0f, 0.0f, 0.0f, 1.0f };
-	diffuse_ = glm::fvec4{ 0.0f, 1.0f, 0.0f, 1.0f };
-	specular_ = glm::fvec4{ 0.0f, 0.0f, 1.0f, 1.0f };
+	ambient_ = glm::fvec4{ 1.0f, 1.0f, 1.0f, 1.0f };		// GL_AMBIENT
+	diffuse_ = glm::fvec4{ 1.0f, 1.0f, 1.0f, 1.0f };		// GL_DIFFUSE
+	specular_ = glm::fvec4{ 1.0f, 1.0f, 1.0f, 1.0f };		// GL_SPECULAR
 
-	position_ = glm::fvec4{ 50.0f, -47.0f, 30.0f ,1.0};
-	direction_ = glm::fvec4{ 0.5f, .0f, -1.0f, 1.0 };
+	position_ = glm::fvec4{ 50.0f, -47.0f, 30.0f ,1.0};		// GL_SPOT_POSITION
+	direction_ = glm::fvec4{ 0.5f, .0f, -1.0f, 1.0 };		// GL_SPOT_DIRECTION
 
-	spotExponent_ = 1.0f;			               // GL_SPOT_EXPONENT
-	spotCutoffAngle_ = 15.0f;                    // GL_SPOT_CUTOFF 
-	attenuation_[0] = 1.0f;              // GL_CONSTANT_ATTENUATION
-	attenuation_[1] = 0.0f;              // GL_LINEAR_ATTENUATION
-	attenuation_[2] = 0.0f;
+	spotExponent_ = 1.0f;			// GL_SPOT_EXPONENT
+	spotCutoffAngle_ = 30.0f;		// GL_SPOT_CUTOFF 
+	attenuation_[0] = 1.0f;			// GL_CONSTANT_ATTENUATION
+	attenuation_[1] = 0.0f;			// GL_LINEAR_ATTENUATION
+	attenuation_[2] = 0.0f;			// GL_QUADRATI_ATTENUATION
 }
 
 ////////////////////////////////////////////////////////////////////////
