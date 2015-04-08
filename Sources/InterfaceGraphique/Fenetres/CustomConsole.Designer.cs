5a09dc7975433c03ec0b87737931f89b9d1990a2
Aymen Djellal <aymendjellal@gmail.com>
2015-04-08 00:18:31 -0400

    UI de la console ameliorer

--- Sources/InterfaceGraphique/Fenetres/CustomConsole.Designer.cs
+++ Sources/InterfaceGraphique/Fenetres/CustomConsole.Designer.cs
@@ -34,13 +34,14 @@ namespace InterfaceGraphique
             this.DeleteHistory = new System.Windows.Forms.Button();
             this.ClearConsole = new System.Windows.Forms.Button();
             this.CloseConsole = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.pause_resume = new System.Windows.Forms.CheckBox();
             this.Always_Button = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // ShowHistory
             // 
+            this.ShowHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.ShowHistory.Location = new System.Drawing.Point(12, 411);
             this.ShowHistory.Name = "ShowHistory";
             this.ShowHistory.Size = new System.Drawing.Size(103, 23);
@@ -51,6 +52,7 @@ namespace InterfaceGraphique
             // 
             // DeleteHistory
             // 
+            this.DeleteHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.DeleteHistory.Location = new System.Drawing.Point(121, 412);
             this.DeleteHistory.Name = "DeleteHistory";
             this.DeleteHistory.Size = new System.Drawing.Size(91, 23);
@@ -61,6 +63,7 @@ namespace InterfaceGraphique
             // 
             // ClearConsole
             // 
+            this.ClearConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ClearConsole.Location = new System.Drawing.Point(543, 412);
             this.ClearConsole.Name = "ClearConsole";
             this.ClearConsole.Size = new System.Drawing.Size(75, 23);
@@ -71,6 +74,7 @@ namespace InterfaceGraphique
             // 
             // CloseConsole
             // 
+            this.CloseConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CloseConsole.Location = new System.Drawing.Point(624, 412);
             this.CloseConsole.Name = "CloseConsole";
             this.CloseConsole.Size = new System.Drawing.Size(75, 23);
@@ -79,23 +83,9 @@ namespace InterfaceGraphique
             this.CloseConsole.UseVisualStyleBackColor = true;
             this.CloseConsole.Click += new System.EventHandler(this.Close_Click);
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.BackColor = System.Drawing.Color.Black;
-            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
-            this.richTextBox1.HideSelection = false;
-            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.ShortcutsEnabled = false;
-            this.richTextBox1.Size = new System.Drawing.Size(687, 393);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
             // pause_resume
             // 
+            this.pause_resume.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.pause_resume.Appearance = System.Windows.Forms.Appearance.Button;
             this.pause_resume.AutoSize = true;
             this.pause_resume.Location = new System.Drawing.Point(253, 412);
@@ -108,6 +98,7 @@ namespace InterfaceGraphique
             // 
             // Always_Button
             // 
+            this.Always_Button.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.Always_Button.AutoSize = true;
             this.Always_Button.Location = new System.Drawing.Point(379, 416);
             this.Always_Button.Name = "Always_Button";
@@ -115,6 +106,23 @@ namespace InterfaceGraphique
             this.Always_Button.TabIndex = 9;
             this.Always_Button.Text = "Show Always";
             this.Always_Button.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ShortcutsEnabled = false;
+            this.richTextBox1.Size = new System.Drawing.Size(687, 393);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // CustomConsole
             // 
@@ -128,6 +136,7 @@ namespace InterfaceGraphique
             this.Controls.Add(this.ClearConsole);
             this.Controls.Add(this.DeleteHistory);
             this.Controls.Add(this.ShowHistory);
+            this.MinimumSize = new System.Drawing.Size(720, 400);
             this.Name = "CustomConsole";
             this.Text = "CustomConsole";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Close_External);
@@ -143,8 +152,8 @@ namespace InterfaceGraphique
         private System.Windows.Forms.Button DeleteHistory;
         private System.Windows.Forms.Button ClearConsole;
         private System.Windows.Forms.Button CloseConsole;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.CheckBox pause_resume;
         private System.Windows.Forms.CheckBox Always_Button;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
